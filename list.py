f98bfbb6950b582d838a33dc6379e6e51372a3d2
felly-kakan <theresiamusyoka1@gmail.com>
2023-10-30 16:22:05 +0300

    code addedddd

---
+++ list.py
@@ -0,0 +1,16 @@
+numbers=[4,2,3,8,1,10]
+#append adding new number in list
+numbers.append(6)
+#sort -used to sort from least 
+numbers.sort()
+#inserting you have to nsert the position and the number(index,number)
+numbers.insert(7,12)
+#removes the number you want to remove
+numbers.remove(8) 
+#pop removes the last number
+numbers.pop()
+#clear removes all the values from the list but you dont have to specify the numbers to be removed
+numbers.clear()
+
+print(numbers)
+
